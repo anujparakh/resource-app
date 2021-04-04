378307fd29e9573da65c80e9bc9e30fc9a34d60a
anujparakh <anujparakh@users.noreply.github.com>
2021-04-04 23:27:42 +0000

    deploy: 2986e74d92dfa6420d1aad98eb3db8239a154806

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+var markersList = JSON.parse(markers);
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+
+    const centerPos = { lat: markersList[0].lat, lng: markersList[0].long }
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
+        zoom: 11,
+        center: centerPos
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    });
+
+    for(const aMarker of markersList)
+    {
+        const newPos = { lat: aMarker.lat, lng: aMarker.long };
+        const newMarker = new google.maps.Marker({
+            position: newPos,
+            map: map,
+            title: aMarker.title
+        });
+    }
+
+    // // The location of Uluru
+    // const uluru = { lat: -25.344, lng: 131.036 };
+    // // The map, centered at Uluru
+    // const map = new google.maps.Map(document.getElementById("map"), {
+    //     zoom: 4,
+    //     center: uluru,
+    // });
+    // // The marker, positioned at Uluru
+    // const marker = new google.maps.Marker({
+    //     position: uluru,
+    //     map: map,
+    // });
 }
