ba6e4c9e0d95f91689d45c4e46310e8c5ac3cfb1
Anuj Parakh <anuj@anujinfotech.com>
2021-04-05 10:11:25 -0500

    added current location button and spinner

--- view/index.js
+++ view/index.js
@@ -30,31 +30,54 @@ function initMap() {
 
 
 
-    // const locationButton = document.createElement("button");
-    // locationButton.textContent = "Pan to Current Location";
-    // locationButton.classList.add("custom-map-control-button");
-    // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-    // locationButton.addEventListener("click", () => {
-    //     // Try HTML5 geolocation.
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(
-    //             (position) => {
-    //                 const pos = {
-    //                     lat: position.coords.latitude,
-    //                     lng: position.coords.longitude,
-    //                 };
-    //                 // infoWindow.setPosition(pos);
-    //                 // infoWindow.setContent("Location found.");
-    //                 // infoWindow.open(map);
-    //                 map.setCenter(pos);
-    //             },
-    //             () => {
-    //                 // handleLocationError(true, infoWindow, map.getCenter());
-    //             }
-    //         );
-    //     } else {
-    //         // Browser doesn't support Geolocation
-    //         // handleLocationError(false, infoWindow, map.getCenter());
-    //     }
-    // });
+    const locationButton = document.createElement("button");
+    locationButton.classList.add("btn")
+    locationButton.classList.add("btn-primary")
+    locationButton.classList.add("mt-2")
+
+    locationButton.textContent = "Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        document.getElementById("spinner-holder").style.display = 'block'
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+
+                    const newMarker = new google.maps.Marker({
+                        position: pos,
+                        map: map,
+                        title: "You are here",
+                        icon: {
+                            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                            strokeColor: "blue",
+                            scale: 3
+                        },
+                        optimized: false
+                    });
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("You are here");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                    document.getElementById("spinner-holder").style.display = 'none'
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                    document.getElementById("spinner-holder").style.display = 'none'
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+            document.getElementById("spinner-holder").style.display = 'none'
+        }
+    });
+
+
 }
