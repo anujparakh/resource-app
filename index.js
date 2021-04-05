7275311192847fa6e4b57ec2f00041215676a0c5
anujparakh <anujparakh@users.noreply.github.com>
2021-04-05 14:33:07 +0000

    deploy: 220b170c692b64bc2a4136a3013c3fccc8d4f799

--- index.js
+++ index.js
@@ -8,26 +8,51 @@ function initMap() {
         center: centerPos
     });
 
-    for(const aMarker of markersList)
-    {
+    const infoWindow = new google.maps.InfoWindow();
+
+
+    for (const aMarker of markersList) {
         const newPos = { lat: aMarker.lat, lng: aMarker.long };
         const newMarker = new google.maps.Marker({
             position: newPos,
             map: map,
-            title: aMarker.title
+            title: aMarker.title,
+            optimized: false
         });
+        newMarker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(newMarker.getTitle());
+            infoWindow.open(newMarker.getMap(), newMarker);
+          });
     }
 
-    // // The location of Uluru
-    // const uluru = { lat: -25.344, lng: 131.036 };
-    // // The map, centered at Uluru
-    // const map = new google.maps.Map(document.getElementById("map"), {
-    //     zoom: 4,
-    //     center: uluru,
-    // });
-    // // The marker, positioned at Uluru
-    // const marker = new google.maps.Marker({
-    //     position: uluru,
-    //     map: map,
+
+
+    // const locationButton = document.createElement("button");
+    // locationButton.textContent = "Pan to Current Location";
+    // locationButton.classList.add("custom-map-control-button");
+    // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    // locationButton.addEventListener("click", () => {
+    //     // Try HTML5 geolocation.
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentPosition(
+    //             (position) => {
+    //                 const pos = {
+    //                     lat: position.coords.latitude,
+    //                     lng: position.coords.longitude,
+    //                 };
+    //                 // infoWindow.setPosition(pos);
+    //                 // infoWindow.setContent("Location found.");
+    //                 // infoWindow.open(map);
+    //                 map.setCenter(pos);
+    //             },
+    //             () => {
+    //                 // handleLocationError(true, infoWindow, map.getCenter());
+    //             }
+    //         );
+    //     } else {
+    //         // Browser doesn't support Geolocation
+    //         // handleLocationError(false, infoWindow, map.getCenter());
+    //     }
     // });
 }
